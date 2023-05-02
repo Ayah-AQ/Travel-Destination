836846cd679d21f49625b50ded7170c8ef63d8e6
Ayah-AQ <ayoya_1998@hotmail.com>
2023-05-02 10:10:41 +0000

    lab 12

--- src/components/TourDetails/TourDetails.js
+++ src/components/TourDetails/TourDetails.js
@@ -4,6 +4,7 @@ import Header from "../Header";
 import Footer from "../Footer";
 import data from '../../data/db.json'
 import { BodySt, DistS} from "../../style";
+import './Detail.css'
 export default function TourDetails() {
 
     const [showMore, setShowMore] = useState(false);
@@ -12,10 +13,10 @@ export default function TourDetails() {
     return (
         <>
         <Header/>
-       <BodySt>
+       <div className="bodyS">
             {/* <PS>{dataFilter[0].id}</PS> */}
             <h2>{dataFilter[0].name}</h2>
-            <DistS src={dataFilter[0].image} alt="img"/>
+            <img className="imgS" src={dataFilter[0].image} alt="img"/>
 
       <h3>
         {showMore ? dataFilter[0].info : `${dataFilter[0].info.substring(0, 250)}`}
@@ -24,7 +25,7 @@ export default function TourDetails() {
         </button>
         </h3>
             <h3>price: {dataFilter[0].price}$</h3>
-        </BodySt>       
+        </div>  
         <Footer/>
       </>
     )
